da94eb70246d6dec29025373c5f69892599fd35e
MassFrat <148748075+MassFrat@users.noreply.github.com>
2025-05-19 19:39:55 +0800

    Update get_service_monitor.sh

--- roles/nginx/files/get_service_monitor.sh
+++ roles/nginx/files/get_service_monitor.sh
@@ -1,17 +1,12 @@
-#!/bin/bash/
+#!/bin/bash
 
 # Collect System Metrics as a Percentage
-CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
-MEMORY_USAGE=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
-DISK_USAGE=$(df -h / | awk 'NR==2 {print $5}')
-UPTIME=$(uptime -p)
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+memory_usage=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+disk_usage=$(df -h / | awk 'NR==2 {print $5}')
+uptime=$(uptime -p)
 
-# Display the Report
-echo ""
-echo "System Monitoring Report - $(date)"
-echo "---------------------------------"
-echo "CPU Usage: $CPU_USAGE%"
-echo "Memory Usage: $MEMORY_USAGE%"
-echo "Disk Usage: $DISK_USAGE"
-echo "Uptime: $UPTIME"
-echo ""
+echo "CPU Usage: $cpu_usage"
+echo "Memory Usage: $memory_usage"
+echo "Disk Usage: $disk_usage"
+echo "Uptime: $uptime"
